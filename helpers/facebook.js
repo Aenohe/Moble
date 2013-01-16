49e9b094fc3840d1cd213bf312389b408c509192
Swibge <matthieu.pinte@gmail.com>
2013-01-16 22:38:40 +0100

    try with fbAsync

--- helpers/facebook.js
+++ helpers/facebook.js
@@ -2,17 +2,19 @@
 define(['facebookSDK'],
   function(FB) {
 
-    FB.init({ appId: moble.facebook.appId });
+    window.fbAsyncInit = function() {
+      FB.init({ appId: moble.facebook.appId });
 
-    FB.Event.subscribe('auth.statusChange', function(res) {
-      if (res.status == 'connected' ) {
-        var auth = { FBId: res.authResponse.userID, token: res.authResponse.accessToken };
-        moble.trigger('user:check_connection', auth);
-      }
-      else {
-        moble.trigger('user:disconnected');
-      }
-    });
+      FB.Event.subscribe('auth.statusChange', function(res) {
+        if (res.status == 'connected' ) {
+          var auth = { FBId: res.authResponse.userID, token: res.authResponse.accessToken };
+          moble.trigger('user:check_connection', auth);
+        }
+        else {
+          moble.trigger('user:disconnected');
+        }
+      });
+    };
 
     return FB;
   });
