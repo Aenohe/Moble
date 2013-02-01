7bfb2aae824556beafd984fcf32ac0336654e855
Fabien Gavory <fabien.gavory@onprenduncafe.com>
2013-02-01 15:19:16 +0100

    initial commit

---
+++ moble-server/User.js
@@ -0,0 +1,7 @@
+function User(FBId, socket)
+{
+	this.id = FBId;
+	this.socket = socket;
+}
+
+exports.User = User;
---
+++ moble-server/conf.js
@@ -0,0 +1,7 @@
+
+/*
+  conf.js
+  exports data into object conf by a require
+*/
+
+exports.port = 80
