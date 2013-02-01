97b8f824518a63a443f6832da20e1ce2c0523aa7
Fabien Gavory <fabien.gavory@onprenduncafe.com>
2013-02-01 17:12:23 +0100

    debug

--- moble-server/server.js
+++ moble-server/server.js
@@ -1,8 +1,11 @@
 var conf = require('./conf');
-var io = require('socket.io').listen(conf.port);
+//var io = require('socket.io').listen(conf.port);
 
 //var route = require('./url_routes');
 
-io.sockets.on('connection', function (socket) {
-console.log('toto');
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
 });
+
+io.sockets.on('connection', function (socket) {});
