97131b2a4e5eb47fbfa0aa4dddfbb494f51425a4
Fabien Gavory <fabien.gavory@onprenduncafe.com>
2013-02-01 15:58:49 +0100

    debug

--- moble-server/db_management.js
+++ moble-server/db_management.js
@@ -2,7 +2,7 @@ var users_management = require('./User');
 var mongoose = require('mongoose');
 var https = require('https');
 
-mongoose.connect('mongodb://localhost');
+mongoose.connect('mongodb://localhost:10034');
 var Schema = mongoose.Schema;
 
 mongoose.model('User', new Schema({ 
