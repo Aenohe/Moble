4de7cb6c901a91622f5c3b0b4efa95471458eaf1
Fabien Gavory <fabien.gavory@onprenduncafe.com>
2013-02-01 15:32:43 +0100

    debug

--- moble-server/server.js
+++ moble-server/server.js
@@ -1,6 +1,6 @@
 var conf = require('./conf');
-var io = require('socket.io').listen(conf.port);
 /*
+var io = require('socket.io').listen(conf.port);
 var route = require('./url_routes');
 
 io.sockets.on('connection', function (socket) {
