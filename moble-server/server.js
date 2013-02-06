d69b7c67ea1f0115caedd663ceae93c67f2ff0e7
Fabien Gavory <fabien.gavory@onprenduncafe.com>
2013-02-06 15:07:56 +0100

    dev moble-client

--- moble-server/server.js
+++ moble-server/server.js
@@ -6,6 +6,7 @@ var conf = require('./conf'),
 //var route = require('./url_routes');
 
 app.configure(function () {
+  app.set("view options", {layout: false});
   app.use(express.errorHandler());
   app.use(express.static(__dirname + '../moble-client'));
 });
@@ -18,6 +19,10 @@ io.configure(function () {
 var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
 app.listen(port, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+
+app.get('/', function(req, res) {
+    res.render('index.html');
 });
 
 io.on('connection', function (socket) {
