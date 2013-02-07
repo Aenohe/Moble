6772ef84d266ec38195f062a827d266faa112a86
Fabien Gavory <fabien.gavory@onprenduncafe.com>
2013-02-07 17:29:16 +0100

    die hard

--- moble-client/views/profile.js
+++ moble-client/views/profile.js
@@ -56,7 +56,7 @@ define(['jquery', 'underscore', 'backbone', 'handlebars', 'text!templates/profil
           },
           inviting: function () {
             alert('toto');
-            console.log($(this).children('img:first').attr('id'));
+            console.log($('img:first', this).attr('id'));
             /*
             FB.ui({method: 'apprequests',
               message: 'Moble is great !',
