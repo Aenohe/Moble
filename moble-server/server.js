9b29b77ef867f2010ebed45ce0f61ea13b4041e7
Fabien Gavory <fabien.gavory@onprenduncafe.com>
2013-02-07 19:19:36 +0100

    conflicts fixed

--- moble-server/server.js
+++ moble-server/server.js
@@ -2,9 +2,9 @@ var express = require('express'),
     app = express.createServer(express.logger()),
     io = require('socket.io').listen(app);
 
-    console.log('******');
+    console.log('******\n');
 console.log(process.env.MONGOHQ_URL);
-console.log('************');
+console.log('************\n');
 
 var route = require('./url_routes');
 
